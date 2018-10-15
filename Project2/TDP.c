9d7eac75eea48769496d6941c7aaa212033cf941
顾俊康 <junkanggu@dhcp-10-4-9-213.wireless.rochester.edu>
2018-10-14 21:25:55 -0400

    Part 2 bug fixed

--- Project2/TDP.c
+++ Project2/TDP.c
@@ -14,7 +14,7 @@ TDP new_TDP(char * input){
     TDP this=(TDP)malloc(sizeof(struct TDP));
     this->input=input;
     this->root=newNode('E');
-    execute(this);
+    this->root=execute(this);
     return this;
 }
 
@@ -40,14 +40,14 @@ char terminal[]={'0','1','2','3','4','5','6','7','8','9','+','-','*','/','(',')'
 char productionLeft[]={'E','t','t','t','T','f','f','f','F','F','N','n','n','D','D','D','D','D','D','D','D','D','D'};
 char* productionRight[]={"Tt","+Tt","-Tt","e","Ff","*Ff","/Ff","e","N","(E)","Dn","N","e","0","1","2","3","4","5","6","7","8","9"};
 int table[8][17] = {
-        //0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -, *, /, (, ), e
-        /*E*/{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1,-1,-1,-1,1,-1,-1 },
+            //0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -, *, /, (, ), e
+        /*E*/{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1,-1,-1,-1,0,0,0},
         /*T*/{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, -1,-1, -1,-1,4,-1,-1},
         /*t*/{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1, 2, -1,-1,-1,-1,3},
-        /*F*/{8, 8, 8, 8, 8, 8,8, 8, 8, 8, -1,-1,-1,-1,9,-1 , -1},
+        /*F*/{8, 8, 8, 8, 8, 8,8, 8, 8, 8, -1,-1,-1,-1,9,-1,-1},
         /*f*/{-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,-1,5,6,-1,-1,7},
         /*N*/{10,10,10,10,10,10,10,10,10,10,-1,-1,-1,-1,-1,-1,-1},
-        /*n*/{11,11,11,11,11,11,11,11,11,11,-1,-1,-1,-1,-1,-1,12},
+        /*n*/{11,11,11,11,11,11,11,11,11,11,-1,-1,-1,-1,-1,-1,12 },
         /*D*/{13,14,15,16,17,18,19,20,21,22,-1,-1,-1,-1,-1,-1,-1}
         
     };
@@ -69,6 +69,8 @@ int tdp_findTerminal(char c){
 char* production(int r, int c){
     int id=table[r][c];
     if(id==-1){
+        id=table[r][16];
+        if(id==-1)
         return NULL;
     }
     return productionRight[id];
@@ -81,25 +83,22 @@ Node execute(TDP tdp){
     
     while(stack->size!=0){
         char goal=pop(stack);
-        printf("%s%d%s","goal: ", goal, "  " );
         if(tdp_findTerminal(goal)!=-1){//is terminal, match and consume
+            
             tdp_matchTerminal(tdp, goal);
+            
         }else if(tdp_findNonTerminal(goal)!=-1){//is not a terminal
-            char input=tdp_lookahead2(tdp);
-            // printf("%s%d%s","input: ", input, "  " );
-            int r=tdp_findNonTerminal(goal);
             
-            printf("%s%d%s","r: ", r, " ");
+            char input=tdp_lookahead2(tdp);//lookahead
+            int r=tdp_findNonTerminal(goal);//
             int c=tdp_findTerminal(input);
-            printf("%s%d%s","c:" , c, " ");
-            
             if(c==-1) c=16;//e
             char* produc=production(r, c);
             if(produc==NULL)return NULL;
             pushString(stack, produc);
             Node leaf=findLeftMostNonterminalLeaf(tdp->root);
-            
             Node_addChildrenByString(leaf, produc);
+            
         }else{
             return NULL;
         }
